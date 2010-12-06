6d39afcef1dcf87f9080e38af435bf3848d58351
jafar.bij.ict <jafar.bij.ict@gmail.com>
2010-12-06 12:30:29 +0000

    Added Snail Changed load_image to return just the image

---
+++ prototype/src/snail.py
@@ -0,0 +1,9 @@
+import pygame
+
+from utils import load_image
+
+class Snail(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = load_image('sprites/snail.png'')
+        self.rect = self.image.get_rect()
--- prototype/src/utils.py
+++ prototype/src/utils.py
@@ -19,7 +19,7 @@ def load_image(name, colorkey=None):
         if colorkey is -1:
             colorkey = image.get_at((0,0))
         image.set_colorkey(colorkey, RLEACCEL)
-    return image, image.get_rect()
+    return image
 
 def load_sound(name):
     """loads a sound file (.wav) in memory"""
