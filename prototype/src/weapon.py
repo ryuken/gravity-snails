b62b0aacfa27ea086f4de571759a19d0fac306b3
sammiestoel <sammiestoel@gmail.com>
2010-12-07 13:53:46 +0000

    added Bazooko Icon and changed Weapon to Item

--- prototype/src/weapon.py
+++ prototype/src/weapon.py
@@ -3,7 +3,7 @@ import sys
 from pygame.locals import *
 from utils import load_image
 
-class Weapon(pygame.sprite.Sprite):
+class Item(pygame.sprite.Sprite):
 
     def __init__(self):
         self.power = 20
@@ -11,10 +11,10 @@ class Weapon(pygame.sprite.Sprite):
     def shoot(self, target):
         target.getHit(self)
 
-class Bazooka(Weapon):
+class Bazooka(Item):
     
     def __init__(self):
-        Weapon.__init__(self)
+        Item.__init__(self)
         self.image = load_image('bazooka_icon.png')
         self.rect = self.image.get_rect()
         
