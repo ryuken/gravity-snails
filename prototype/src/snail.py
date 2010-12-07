dd44c72f58bc7c02e62360372c4234ac5040c0f6
jafar.bij.ict <jafar.bij.ict@gmail.com>
2010-12-07 10:16:39 +0000

    Snail can walk to right and left

--- prototype/src/snail.py
+++ prototype/src/snail.py
@@ -1,4 +1,6 @@
 import pygame
+import sys
+from pygame.locals import *
 
 from utils import load_image
 
@@ -7,3 +9,13 @@ class Snail(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = load_image('snail.png')
         self.rect = self.image.get_rect()
+        self.left_pressed = False
+        self.right_pressed = False
+        self.up_pressed = False
+        self.down_pressed = False
+    def update(self):
+        #E Event
+        if (pygame.key.get_pressed()[K_LEFT]):
+            self.rect = self.rect.move(-1,0)
+        if (pygame.key.get_pressed()[K_RIGHT]):
+            self.rect = self.rect.move(1,0)
