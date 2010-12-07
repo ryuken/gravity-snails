e07a584f4aeb3b53898ff7acbd3d5ba3a330c6aa
jafar.bij.ict <jafar.bij.ict@gmail.com>
2010-12-07 11:51:20 +0000

    refactored snail.update

--- prototype/src/snail.py
+++ prototype/src/snail.py
@@ -11,11 +11,19 @@ class Snail(pygame.sprite.Sprite):
         self.image_left = load_image('snailLeft.png')
         self.image = self.image_right
         self.rect = self.image.get_rect()
-        self.speed = {'x' : 5, 'y' : 5}
+        self.speed = {'x' : 2, 'y' : 5}
         self.direction = {'x' : 0.0, 'y' : 0.0}#Don't touch this!!!
         self.terrain = terrain
     def update(self):
         #E Event
+        self.updateMove()
+        self.updateGravity()
+
+        self.rect = self.rect.move(self.direction['x'], self.direction['y'])
+
+        self.updateCollision()
+
+    def updateMove(self):
         self.direction['x'] = 0
         if (pygame.key.get_pressed()[K_LEFT]):
             self.direction['x'] = -self.speed['x']
@@ -23,13 +31,16 @@ class Snail(pygame.sprite.Sprite):
         if (pygame.key.get_pressed()[K_RIGHT]):
             self.direction['x'] = self.speed['x']
             self.image = self.image_right
-        if (pygame.key.get_pressed()[K_RETURN]):
-            self.direction['y'] = -self.speed['y']
+
+    def updateGravity(self):
         self.direction['y'] += 0.2
         if self.direction['y'] > 5:
             self.direction['y'] = 5
-        self.rect = self.rect.move(self.direction['x'], self.direction['y'])
+
+    def updateCollision(self):
         list = pygame.sprite.spritecollide(self, self.terrain, False)
         if(len(list) > 0):
             self.rect = self.rect.move(-self.direction['x'], -self.direction['y'])
             self.direction['y'] = 0
+            if (pygame.key.get_pressed()[K_RETURN]):
+                self.direction['y'] = -self.speed['y']
