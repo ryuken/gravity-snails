319d00e4b377787927eda71280881a4984b4cdf9
sammiestoel <sammiestoel@gmail.com>
2010-12-07 13:37:27 +0000

    Added Bazooka icon and class to weapon.py

--- prototype/src/weapon.py
+++ prototype/src/weapon.py
@@ -10,6 +10,12 @@ class Weapon(pygame.sprite.Sprite):
     
     def shoot(self, target):
         target.getHit(self)
+
+class Bazooka(Weapon):
     
-    def getPower(self):
-        return self.power
+    def __init__(self):
+        Weapon.__init__(self)
+        self.image = load_image('bazooka_icon.png')
+        self.rect = self.image.get_rect()
+        
+    
