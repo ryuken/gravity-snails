996805e4d7389a5ae740f161ae0c5d8b82fa1a56
sammiestoel <sammiestoel@gmail.com>
2011-01-04 11:55:28 +0000

    removed teams from constructor moved it to startTimer

--- src/turnmanager.py
+++ src/turnmanager.py
@@ -5,7 +5,14 @@ from settings import Settings
 from threading import Timer
 
 class TurnManager(object):
-    def __init__(self, teams):
+    _instance = None
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Turnmanager, cls).__new__(
+                                cls, *args, **kwargs)
+        return cls._instance
+
+    def __init__(self):
         # Display some text
         pygame.font.init()
         self.font_size = Settings.TIMER_FONT_SIZE
@@ -17,17 +24,18 @@ class TurnManager(object):
         self.size = Settings.TIMER_SIZE
         self.rect = pygame.Rect(self.position, self.size)
         self.status = TurnStatus.BREAK
-        self.teams = teams
+        self.teams = []
         self.currentTeamTurn = 0
-        self.teams[0].hasTurn = True
         self.timer = Timer(1.0, self.updateTime)
-        
-    def startTimer(self):
+
+    def startTimer(self, teams):
+        self.teams = teams
+        self.teams[0].hasTurn = True
         self.timer.start()
-        
+
     def stopTimer(self):
         self.timer.cancel()
-    
+
     def draw(self, surface):
         # Draw the red rectangle on the game surface
         self.rect = surface.fill((255,0,0), self.rect)
@@ -35,11 +43,11 @@ class TurnManager(object):
         # Create a new surface at the position of the red rectangle and draw the text
         text = self.font.render(str(self.currentTime), 1, (10, 10, 10)) #returns surface
         surface.blit(text, (Settings.TIMER_SIZE[0] / 2 - 7, Settings.TIMER_SIZE[1] / 2 - 7))
-           
+
     def updateTime(self):
         self.updateStatus()
         self.currentTime -= 1
-    
+
     def updateStatus(self):
         if self.currentTime == 0:
             if self.status == TurnStatus.BREAK:
@@ -51,12 +59,12 @@ class TurnManager(object):
                 self.changeTurn()
         self.timer = Timer(1.0, self.updateTime)
         self.timer.start()
-                
+
     def changeTurn(self):
         #give the turn to the next team and set the current team on false
         if self.teams[self.currentTeamTurn].hasTurn == True:
             self.teams[self.currentTeamTurn].hasTurn = False
-            
+
             nextTeam = self.currentTeamTurn + 1
             if nextTeam < len(self.teams):
                 self.currentTeamTurn = nextTeam
