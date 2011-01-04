6fc8086a91daab5269cd13e29c3f3c75bbe7d69e
sammiestoel <sammiestoel@gmail.com>
2011-01-04 11:58:53 +0000

    captial m in super(TurnManager)

--- src/turnmanager.py
+++ src/turnmanager.py
@@ -8,7 +8,7 @@ class TurnManager(object):
     _instance = None
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
-            cls._instance = super(Turnmanager, cls).__new__(
+            cls._instance = super(TurnManager, cls).__new__(
                                 cls, *args, **kwargs)
         return cls._instance
 
