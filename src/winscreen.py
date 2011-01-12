a0b4229f61093cde873868b05b09647fb08f4430
Eurojumbo <Eurojumbo@gmail.com>
2011-01-12 13:01:20 +0000

    win screen geupdate

--- src/winscreen.py
+++ src/winscreen.py
@@ -2,19 +2,34 @@ import pygame
 from scenemanager import SceneManager
 from gui.button import Button
 from gui.label import Label
-from menu import Menu
+from gui.frame import Frame
+from scene import Scene
 from settingsmenu import SettingsMenu
 
-class WinScreen(Menu):
+class WinScreen(Scene):
     def __init__(self, nextScene, teamColor):
-        Menu.__init__(self)
+        Scene.__init__(self)
         self.nextScene = nextScene
-        self.addWidget(Label("!!! Congratulations !!!"))
-        self.addWidget(Label(" "))
-        self.teamColor = teamColor
-        self.addWidget(Label("The "+self.teamColor+" team has won"))
-        self.addWidget(Label(" "))
-        self.addWidget(Button("BACK", self.runNextScene))
+        self.frame = Frame()
+        text = "!!! Congratulations !!!\n"
+        text += "\n"
+        text += "The " + teamColor + " team has won"
+        labelText = Label(text)
+        labelText.rect.center = (self.frame.rect.width / 2, self.frame.rect.height / 2)
+        
+        buttonBack = Button("Back", self.runNextScene)
+        buttonBack.rect.size = (128, 32)
+        buttonBack.rect.centerx = self.frame.rect.width / 2
+        buttonBack.rect.bottom = self.frame.rect.height - 32
+        
+        self.frame.addWidget(labelText)
+        self.frame.addWidget(buttonBack)
 
+    def draw(self, surface):
+        self.frame.draw(surface)
+        
+    def update(self, input):
+        self.frame.update(input)
+    
     def runNextScene(self):
         SceneManager().setScene(self.nextScene)
