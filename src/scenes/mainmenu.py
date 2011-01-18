6b91368c2691136934d656fb68958554f06f2f34
jafar.bij.ict <jafar.bij.ict@gmail.com>
2011-01-18 17:05:00 +0000

    added documentation to the main menu

--- src/scenes/mainmenu.py
+++ src/scenes/mainmenu.py
@@ -8,6 +8,9 @@ from scenes.help import HelpMenu
 
 class MainMenu(Scene):
     def __init__(self):
+        """
+        @summary: Initializes a main menu
+        """
         Scene.__init__(self)
         self.helpMenu = HelpMenu(self)
 
@@ -18,21 +21,41 @@ class MainMenu(Scene):
         self.menu.addWidget(Button("Quit", self.quitGame))
 
     def draw(self, surface):
+        """
+        @param surface: The surface which the main menu should be drawed on
+        @summary: draws the main menu on a specified surface
+        """
         self.menu.draw(surface)
 
     def update(self, input):
+        """
+        @param input: The user input
+        @summary: updates the status of the main menu based on the user input
+        """
         self.menu.update(input)
 
     def runGame(self):
+        """
+        @summary: runs a new game
+        """
         game = Game()
         game.mainmenu = self
         SceneManager().setScene(game)
 
     def runSettingsMenu(self):
+        """
+        @summary: shows the settings menu
+        """
         SceneManager().setScene(SettingsMenu(self))
 
     def runHelpMenu(self):
+        """
+        @summary: shows the help screen
+        """
         SceneManager().setScene(self.helpMenu)
 
     def quitGame(self):
+        """
+        @summary: quits the game
+        """
         SceneManager().setScene(None) #No scene, means quit game!
